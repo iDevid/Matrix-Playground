15bfc4d8296c861fc2892c8046020911d4a7c70f
Davide Sibilio <davide.sibilio@gmail.com>
2017-03-18 14:43:15 +0100

    Added Random Color Value

--- WWDC.playground/Sources/MatrixLabel.swift
+++ WWDC.playground/Sources/MatrixLabel.swift
@@ -6,12 +6,16 @@ public class MatrixLabel: SKLabelNode {
     let values = ["","","","","","","","","",
                   "","","","","","","","","",
                   "","","","","",""]
+    let colorValues = [NSColor(red: 0, green: 255, blue: 0, alpha: 0.9),
+                       NSColor(red: 0, green: 255, blue: 0, alpha: 0.8),
+                       NSColor(red: 0, green: 255, blue: 0, alpha: 0.6)]
     
     public init (size: Int, position: CGPoint) {
         super.init()
         self.fontName = "mCode15"
         self.text = values.random
-        self.fontColor = NSColor.green
+        self.fontColor = colorValues.random
+        self.color = self.fontColor
         self.position = position
         self.fontSize = CGFloat(size)
     }
