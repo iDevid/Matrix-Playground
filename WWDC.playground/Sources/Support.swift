9e333b60a3f9978d30829cb265ac43531a4cfb64
Davide Sibilio <davide.sibilio@gmail.com>
2017-03-19 23:24:26 +0100

    modified random Array extension

--- WWDC.playground/Sources/Support.swift
+++ WWDC.playground/Sources/Support.swift
@@ -3,8 +3,7 @@ import Foundation
 
 public extension Array {
     var random: Element {
-        let index = Int(arc4random_uniform(UInt32(count)))
-        return self[index]
+        return self[Int(arc4random_uniform(UInt32(count)))]
     }
 }
 
