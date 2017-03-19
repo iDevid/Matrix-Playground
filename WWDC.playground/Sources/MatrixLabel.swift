94ca1eeb27c8eb28eb989bcd93bcd075f421c13b
Davide Sibilio <davide.sibilio@gmail.com>
2017-03-19 01:06:48 +0100

    Added startToFade Label Function

--- WWDC.playground/Sources/MatrixLabel.swift
+++ WWDC.playground/Sources/MatrixLabel.swift
@@ -9,7 +9,6 @@ public class MatrixLabel: SKLabelNode {
     let colorValues = [NSColor(red: 0, green: 255, blue: 0, alpha: 0.9),
                        NSColor(red: 0, green: 255, blue: 0, alpha: 0.8),
                        NSColor(red: 0, green: 255, blue: 0, alpha: 0.6),
-                       //* New Colors */
                        NSColor(red: 100, green: 250, blue: 100, alpha: 0.9),
                        NSColor(red: 150, green: 250, blue: 150, alpha: 0.7),
                        NSColor(red: 255, green: 255, blue: 255, alpha: 0.9)]
@@ -22,6 +21,12 @@ public class MatrixLabel: SKLabelNode {
         self.color = self.fontColor
         self.position = position
         self.fontSize = CGFloat(size)
+    }
+    
+    public func startToFade() {
+        self.run(SKAction.fadeAlpha(to: 0, duration: 1), completion: {
+            self.removeFromParent()
+        })
     }
     
     required public init?(coder aDecoder: NSCoder) {
